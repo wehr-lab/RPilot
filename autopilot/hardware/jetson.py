e1de1498133e77e6cb060d2e54e8dd21b0d54d55
sneakers-the-rat <JLSaunders987@gmail.com>
2020-03-06 16:36:41 -0500

    jetson digi out

---
+++ autopilot/hardware/jetson.py
@@ -0,0 +1,25 @@
+import Jetson.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
+
+class Digital_Out_Jetson(object):
+
+    states = {
+        0: GPIO.LOW,
+        1: GPIO.HIGH
+    }
+
+    def __init__(self, pin, initial=0):
+        self.pin = pin
+
+        GPIO.setup(pin, GPIO.OUT, self.states[initial])
+
+
+    def set(self, state):
+        if state in self.states.keys():
+            state = self.states[state]
+        elif state in self.states.values():
+            pass
+        else:
+            raise ValueError('Must be one of {} or {}'.format(self.states.keys(), self.states.values()))
+
+        GPIO.output(self.pin, state)
